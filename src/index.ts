c04209f8291f4690022ed97c8cacde18e3995f0a
Alex Ri <40760467+alex-rie@users.noreply.github.com>
2025-10-27 08:22:45 +0100

    refactor: extract calculator exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export * from './calculators/compound-interest'
-export * from './calculators/gross-to-net'
-export * from './calculators/net-policy'
-export * from './calculators/savings'
+export * from './calculators'
 export * from './utils/calculator'
