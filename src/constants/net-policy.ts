a3a5c7f83b8d54f12c9424fd72f52e0b5958ec2f
Alex Ri <40760467+alex-rie@users.noreply.github.com>
2026-06-09 09:12:09 +0200

    feat: update net policy calculations to use income tax year

--- src/constants/net-policy.ts
+++ src/constants/net-policy.ts
@@ -1,11 +1,1 @@
-/**
- * These values are added artificially to offset automatic deductions (such as lump sums and costs)
- * made by the gross-net calculator, which are not relevant for our policy tax calculations.
- * By adding them here, the calculator will subtract them again, resulting in the correct values for our use case.
- * Values as of 2025.
- */
-export const CORRECTION_VALUES = {
-  werbungskostenpauschale: 1_230,
-  beitragsbemessungsgrenze: 96_600,
-  arbeitslosenversicherung: 0.013,
-} as const
+export const INCOME_TAX_YEAR = 2026 as const
